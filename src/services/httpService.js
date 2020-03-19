fa33fcc91aaaf1ae2a3c09466a06673998be585d
amin havasi <pau.ahq@gmail.com>
2020-03-20 00:55:22 +0330

    httpService

---
+++ src/services/httpService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+axios.interceptors.response.use(null, err => {
+    const exceptionError =
+        err.response && err.response.status >= 400 && err.response.status < 500;
+    if (!exceptionError) {
+        alert('something went wrong');
+    }
+    return Promise.reject(err);
+});
+
+export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete
+};
