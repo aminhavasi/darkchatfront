ef0ea5f2d8a7a4bdfb480585e1688eb4bfddce18
amin havasi <pau.ahq@gmail.com>
2020-03-21 14:11:54 +0430

    chatroom

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 class Chat extends Component {
     render() {
-        return <div>chat</div>;
+        return (
+            <div style={{ minHeight: '100vh', backgroundColor: 'black' }}>
+                <nav className="fixed-top">
+                    <a href="#" className="text-success">
+                        logout
+                    </a>
+                </nav>
+                <div
+                    style={{ minHeight: '100vh' }}
+                    className="d-flex justify-content-center align-items-center text-danger"
+                >
+                    <div
+                        style={{
+                            backgroundColor: '#1a1a1a',
+                            width: '80%',
+                            height: '80vh',
+                            overflowY: 'scroll',
+                            wordBreak: 'break-word',
+                            overflowX: 'hidden',
+                            scrollbarColor: '#009900   #001a00'
+                        }}
+                    ></div>
+                </div>
+            </div>
+        );
     }
 }
 
